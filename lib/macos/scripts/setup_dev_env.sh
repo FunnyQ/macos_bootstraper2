1a26c3a7c4cefa52de1bae560c1e0651e363fcd1
FunnyQ <funnyq@gmail.com>
2020-02-04 02:44:25 +0800

    :sparkles: add mas apps

--- lib/macos/scripts/setup_dev_env.sh
+++ lib/macos/scripts/setup_dev_env.sh
@@ -33,3 +33,5 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 info_echo "Install Brew formalue"
 brew tap "Homebrew/bundle" 2> /dev/null
 brew bundle --file=$1
+
+brew doctor && brew update && brew cleanup && brew upgrade && brew cask upgrade && mas upgrade
